2fb987bb3c23479e86da26d3ab1213375a5ae4a8
Guillaume ARM <trapcodien@gmail.com>
2017-11-19 18:57:00 +0100

    Add css modules support
    using babel-plugin-react-css-modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 /* eslint-env node */
 const path = require('path');
+const context = path.resolve(__dirname, 'src');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './src/boot/index.html',
+  template: './boot/index.html',
   filename: 'index.html',
   inject: 'body'
 });
+
 module.exports = {
-    entry: './src/boot/index.js',
+    context,
+    entry: './boot/index.js',
     output: {
        path: path.join(__dirname, './dist'),
        filename: 'app.bundle.js',
@@ -18,8 +21,15 @@ module.exports = {
     },
     module: {
       loaders: [
-        { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-        { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+        { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
+        {
+          include: context,
+          loaders: [
+            'style-loader',
+            'css-loader?importLoader=1&modules&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+          ],
+          test: /\.css$/
+        },
       ]
     },
     plugins: [HtmlWebpackPluginConfig]
