dfb019456833621b520bbd51ba56a0651a7dc062
Guillaume ARM <trapcodien@gmail.com>
2017-12-10 22:10:01 +0100

    Spawn server process in e2e tests

--- e2e/index.js
+++ e2e/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+const { spawn } = require('child_process');
 const { promisify } = require('util');
 const execFile = promisify(require('child_process').execFile);
 const { pipe, equals } = require('ramda');
@@ -6,6 +7,7 @@ const path = require('path')
 const { readdirSync } = require('fs');
 
 const TEST_FOLDER = 'tests';
+const SERVER_PORT = 8081;
 
 const integrationTests = readdirSync(path.join(__dirname, TEST_FOLDER))
   .filter(pipe(path.extname, equals('.js')))
@@ -13,27 +15,42 @@ const integrationTests = readdirSync(path.join(__dirname, TEST_FOLDER))
   .map(require)
 ;
 
+const env = { ...process.env, NODE_ENV: 'production' }
+
 const webpackArguments = [
   '--output-path', path.join(__dirname, './dist'),
 ];
 
-const webpackOptions = {
-  env: { ...process.env, NODE_ENV: 'production' },
-};
+const serverProcessArguments = [
+  path.join(__dirname, '../src/server/index.js'),
+  '-p', SERVER_PORT,
+]
 
 describe('e2e testing', () => {
+  let serverProcess;
   test('build client (production mode)', async () => {
     try {
-      await execFile('webpack', webpackArguments, webpackOptions);
+      await execFile('webpack', webpackArguments, { env });
     } catch (e) {
       console.warn(e.stdout);
       throw new Error('Error: webpack build failed')
     }
   });
 
-  test('fork server process (production mode)');
+  test('spawn server process (production mode)', (done) => {
+    serverProcess = spawn('node', serverProcessArguments, { env })
+    serverProcess.stdout.on('data', (data) => {
+      expect(data.toString()).toMatch(new RegExp(`^Listening on ${SERVER_PORT}`))
+      done();
+    })
+  });
 
   integrationTests.forEach(([...args]) => {
     test(...args)
+  })
+
+  test('exit server process', () => {
+    serverProcess.stdin.pause()
+    serverProcess.kill();
   })
 })
