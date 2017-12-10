97495b168f7de61bcb8cf021e22bc65ef982bc7f
Guillaume ARM <trapcodien@gmail.com>
2017-12-10 13:32:42 +0100

    use the bootProd() function in client when NODE_ENV=test

--- src/client/boot/index.js
+++ src/client/boot/index.js
@@ -3,7 +3,10 @@ import render from './render';
 import bootDev from './boot.dev';
 import bootProd from './boot.prod';
 
-if (process.env.NODE_ENV === 'production') {
+const isProduction = process.env.NODE_ENV === 'production'
+const isTest = process.env.NODE_ENV === 'test'
+
+if (isProduction || isTest) {
   bootProd();
 } else {
   bootDev();
