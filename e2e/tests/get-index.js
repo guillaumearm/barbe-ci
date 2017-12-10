7e257c948578ff658f90b9810e5e5cf1334d960b
Guillaume ARM <trapcodien@gmail.com>
2017-12-10 22:47:53 +0100

    Implement first e2e test

--- e2e/tests/get-index.js
+++ e2e/tests/get-index.js
@@ -1,3 +1,15 @@
-module.exports = () => ['get index.html', () => {
-  expect(true).toBe(true)
-}]
+const puppeteer = require('puppeteer');
+
+module.exports = ({ SERVER_PORT }) => ['get index.html', async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(`http://localhost:${SERVER_PORT}`);
+
+  const bodyHandle = await page.$('body div#root-container');
+  const html = await page.evaluate(body => body.innerHTML, bodyHandle);
+  await bodyHandle.dispose();
+
+  expect(html).toBeTruthy();
+
+  await browser.close();
+}, 30 * 1000]
