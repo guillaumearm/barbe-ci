e2b1e0ad217e45e097875064dd1651b0169200bb
Guillaume ARM <trapcodien@gmail.com>
2017-12-26 00:44:58 +0100

    Remove console.log in auth routes
    + restyling

--- packages/server/src/middlewares/router.js
+++ packages/server/src/middlewares/router.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 const passport = require('koa-passport');
 const createRouter = require('koa-router');
 const api = require('../api');
@@ -9,52 +8,43 @@ router.post('/api', api);
 
 /* ### AUTH ### */
 
-router.get(
-  '/auth/bitbucket/callback',
+router.get('/auth/bitbucket/callback',
   passport.authenticate('bitbucket', {
     successRedirect: '/auth/success',
     failureRedirect: '/auth/failure'
   })
 )
 
-router.get(
-  '/auth',
-  async ctx => {
-    ctx.body = 'OK'
-    if (ctx.isAuthenticated()) {
-      return ctx.redirect('/auth/success');
-    } else {
-      return passport.authenticate('bitbucket')(ctx)
-    }
-  },
-)
-
-router.get(
-  '/auth/logout',
-  async (ctx) => {
-    ctx.logout()
-    ctx.type = 'html';
-    ctx.body = 'DISCONNECTED - <a href="/auth">Click here to connect</a>'
+router.get('/auth', async ctx => {
+  ctx.body = 'OK'
+  if (ctx.isAuthenticated()) {
+    return ctx.redirect('/auth/success');
+  } else {
+    return passport.authenticate('bitbucket')(ctx)
   }
-)
+})
 
-router.get('/auth/success',
-  async (ctx) => {
-    ctx.type = 'html'
-    if (ctx.isAuthenticated()) {
-      try {
-        const response = await bb.get(ctx, '2.0/repositories/trapcodien/sdp');
-        console.log(response.data.description);
-        ctx.body = 'OK - <a href="/auth/logout">Click here to disconnect</a>'
-      } catch (e) {
-        console.warn(e.message, ':', e.response.data);
-        ctx.redirect('/auth/logout')
-      }
-    } else {
-      ctx.body = 'NOT CONNECTED - <a href="/auth">Click here to connect</a>'
+router.get('/auth/logout', async (ctx) => {
+  ctx.logout()
+  ctx.type = 'html';
+  const message = ctx.query.message || 'DISCONNECTED'
+  ctx.body = `${message} - <a href="/auth">Click here to connect</a>`
+})
+
+router.get('/auth/success', async (ctx) => {
+  ctx.type = 'html'
+  if (ctx.isAuthenticated()) {
+    try {
+      const response = await bb.get(ctx, '2.0/repositories/trapcodien/sdp');
+      ctx.body = `${response.data.slug} : Bitbucket account linked - <a href="/auth/logout">Click here to disconnect</a>`
+    } catch (e) {
+      const message = `${e.message} : ${e.response.data}`
+      ctx.redirect(`/auth/logout?message=${message}`)
     }
-  },
-)
+  } else {
+    ctx.body = 'NOT CONNECTED - <a href="/auth">Click here to connect</a>'
+  }
+})
 
 router.get('/auth/failure', (ctx) => {
   ctx.body = 'login failure'
