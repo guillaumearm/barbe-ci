946cb58d3a09beb6d300065174f1fdea5c713527
Guillaume ARM <trapcodien@gmail.com>
2017-12-31 02:25:11 +0100

    Dispatch reloadBranches only if neccessary

--- packages/server/src/store/modules/db/middleware/bitbucket/reloadRepositories.js
+++ packages/server/src/store/modules/db/middleware/bitbucket/reloadRepositories.js
@@ -2,7 +2,10 @@ module.exports = (store) => (next) => async (action) => {
   if (action.type === 'BITBUCKET_RELOAD_REPOSITORIES') {
     const nextAction = await next(action);
     for (let repository of action.payload.repositories) {
-      await store.bbReloadBranches(repository, store.getBranchesNames({ repository }))
+      const branchNames = store.getBranchesNames({ repository })
+      if (branchNames.length > 0) {
+        await store.bbReloadBranches(repository, branchNames)
+      }
     }
     return nextAction;
   }
