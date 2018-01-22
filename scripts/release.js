4042d0029e01515fa7a49273a859aaef75dc5488
Guillaume ARM <trapcodien@hotmail.fr>
2018-01-22 17:00:28 +0100

    chore(scripts): use yargs in release script

--- scripts/release.js
+++ scripts/release.js
@@ -1,8 +1,21 @@
 const { execSync } = require('child_process');
 const conventionalRecommendedBump = require('conventional-recommended-bump');
 
+const argv = require('yargs')
+  .usage('Usage: $0 [-p]')
+  .example('$0 -p angular', 'start conventional-bump with angular preset')
+  .describe('version', 'Print current version')
+  .help('h')
+    .alias('h', 'help')
+  .alias('p', 'preset')
+    .nargs('p', 1)
+    .default('p', 'angular')
+    .describe('p', 'conventional-changelog preset to use')
+  .argv;
+
+
 const options = {
-  preset: 'angular',
+  preset: argv.preset,
 };
 
 const execNpmVersion = releaseType => (
