8e68227f7dcf1fd36164537a276caf7f7b636416
Guillaume ARM <trapcodien@hotmail.fr>
2018-02-05 20:53:12 +0100

    test(unit): enhance root reducer test
    Ref #3

--- src/store/modules/reducer.spec.js
+++ src/store/modules/reducer.spec.js
@@ -1,7 +1,12 @@
 const reducer = require('./reducer');
 
 describe('store/rootReducer', () => {
+  const unknownAction = { type: 'UNKNOWN' };
+  const initialState = reducer(undefined, unknownAction);
   it('returns initial state', () => {
-    expect(reducer(undefined, { type: 'UNKNOWN' })).toMatchSnapshot();
+    expect(reducer(undefined, unknownAction)).toMatchSnapshot();
+  });
+  it('does not update state on unknown action', () => {
+    expect(reducer(initialState, unknownAction)).toBe(initialState);
   });
 });
