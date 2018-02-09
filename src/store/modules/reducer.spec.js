73aa90a4a6671063a8c9700fd02b3f6e9f67fc1d
Guillaume ARM <trapcodien@hotmail.fr>
2018-02-09 18:33:10 +0100

    test(unit): better initialState for rootReducer

--- src/store/modules/reducer.spec.js
+++ src/store/modules/reducer.spec.js
@@ -2,7 +2,11 @@ const reducer = require('./reducer');
 
 describe('store/reducer', () => {
   const unknownAction = { type: 'UNKNOWN' };
-  const initialState = reducer(undefined, unknownAction);
+  const initialState = {
+    ...reducer(undefined, unknownAction),
+    credentials: { token: 'abcdef' },
+    serverConfiguration: { VERBOSE: true },
+  };
   it('returns initial state', () => {
     expect(reducer(undefined, unknownAction)).toMatchSnapshot();
   });
